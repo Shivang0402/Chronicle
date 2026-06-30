5da06964fe738b6b2b5146933f3c2833ddf88a5e
Victus44 <rennup4288@gmail.com>
2026-06-30 23:44:48 +0530

    Database conncected

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const app = require("./app");
-
 const PORT = process.env.PORT || 3000;
-
+const connectDb = require("./config/db");
+connectDb();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
