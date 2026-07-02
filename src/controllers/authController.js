0158987238450c425be99458ddb02dd56f060656
Victus44 <rennup4288@gmail.com>
2026-07-02 17:03:45 +0530

    Created login route and implemented JTW

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,34 +1,66 @@
 const userModel = require("../models/user");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
   const { name, username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 12);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  const existingUser = await userModel.findOne({
-    $or: [{ username }, { email }],
-  });
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const existingUser = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
 
-  if (existingUser) {
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email already exists" });
+    }
+    const newUser = new userModel({
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
     return res
-      .status(400)
-      .json({ message: "Username or email already exists" });
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
   }
-  const newUser = new userModel({
-    name,
-    username,
-    email,
-    password: hashedPassword,
-  });
-
-  return res
-    .status(201)
-    .json({ message: "User registered successfully", user: newUser });
 };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
 
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Email or Password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    } else {
+      const token = jwt.sign({ id: user._id }, "sessionkey", {
+        expiresIn: "1d",
+      });
+
+      return res.status(200).json({ message: "Login successful", token });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 module.exports = {
   registerUser,
+  loginUser,
 };
