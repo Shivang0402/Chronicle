89b5555f5226058b8e5ed64bf8f4ba7ef0bfe631
Victus44 <rennup4288@gmail.com>
2026-07-02 16:01:49 +0530

    Implemented password hashing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,34 @@
-const registerUser = (req, res) => {
-  res.status(201).json({
-    message: "User registration me bhi sab changa sii!",
+const userModel = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const registerUser = async (req, res) => {
+  const { name, username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const existingUser = await userModel.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "Username or email already exists" });
+  }
+  const newUser = new userModel({
+    name,
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  return res
+    .status(201)
+    .json({ message: "User registered successfully", user: newUser });
 };
+
 module.exports = {
   registerUser,
 };
